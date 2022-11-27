557a3176232fca5b4dd32cfdb3daa7401074c3aa
Evgeniy <elchangizo@yandex.ru>
2022-11-28 02:00:04 +0300

    Initial commit

---
+++ src/test/resources/scripts/outer/CommentLikeRestController/getCommentLikeCount_SuccessfulTest/Before.sql
@@ -0,0 +1,4 @@
+INSERT INTO comment_likes(id, comment_id, user_id, positive) VALUES (1, 1, 1, false);
+INSERT INTO comment_likes(id, comment_id, user_id, positive) VALUES (2, 2, 1, true);
+INSERT INTO comment_likes(id, comment_id, user_id, positive) VALUES (3, 2, 1, false);
+INSERT INTO comment_likes(id, comment_id, user_id, positive) VALUES (4, 3, 1, true);
