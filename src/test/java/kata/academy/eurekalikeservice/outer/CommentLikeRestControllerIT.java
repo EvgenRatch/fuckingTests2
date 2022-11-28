17cc9af5309604a1d9233579a9ec00bafb632e8c
Evgeniy <elchangizo@yandex.ru>
2022-11-28 18:02:09 +0300

    perform2

--- src/test/java/kata/academy/eurekalikeservice/outer/CommentLikeRestControllerIT.java
+++ src/test/java/kata/academy/eurekalikeservice/outer/CommentLikeRestControllerIT.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         @MockBean(ContentServiceFeignClient.class)
 })
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {CommentLikeRestController.class})
 public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
 
     @Autowired
